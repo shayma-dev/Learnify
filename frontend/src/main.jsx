8f24070a0d51c99703625cf7a7aa01de44003b24
Shayma-PM <taqatgaza@gmail.com>
2025-07-30 19:01:48 +0300

    feat: install Mantine and wrap app with MantineProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Import Mantine styles
+// This is required for Mantine components to work properly
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
+//import other package styles as needed
+import { MantineProvider } from '@mantine/core';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MantineProvider /* theme props will go here later */>
+      <App />
+    </MantineProvider>
   </StrictMode>,
 )
