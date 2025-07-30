f983befd0f0d73b3e34b9ce9f839b3de67ada55b
Shayma-PM <taqatgaza@gmail.com>
2025-07-30 18:36:34 +0300

    set up frontend react app using vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Learnify</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+
+export default App;
